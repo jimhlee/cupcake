f39e128ac5018b9cf58707f772b9514f0cc535bb
James Lee <jimhlee23@gmail.com>
2024-03-04 14:43:26 -0500

    complete model for cupcake

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
 """Flask app for Cupcakes"""
+import os
+
+from flask import Flask, jsonify
+# from flask_debugtoolbar import DebugToolbarExtension
+
+from models import connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", 'postgresql:///cupcake')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+# debug = DebugToolbarExtension(app)
+
+
+@app.get('/api/cupcakes')
+def show_cupcakes():
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
--- models.py
+++ models.py
@@ -1,1 +1,52 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Cupcake(db.Model):
+    '''Cupcake model'''
+
+    __tablename__ = 'cupcakes'
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'flavor': self.flavor,
+            'size': self.size,
+            'rating': self.rating,
+            'image_url': self.image_url
+        }
+
+    id = db.Column(
+        db.Integer,
+        primary_key = True,
+        autoincrement = True
+    )
+
+    flavor = db.Column(
+        db.String(50),
+        nullable = False
+    )
+
+    size = db.Column(
+        db.String(15),
+        nullable = False
+    )
+
+    rating = db.Column(
+        db.Integer,
+        nullable = False
+    )
+
+    image_url = db.Column(
+        db.String(500),
+        default = 'https://tinyurl.com/demo-cupcake',
+        nullable = False
+    )
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
